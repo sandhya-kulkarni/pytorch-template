84ba23b615e5a87908a65e2e736010cc81658c03
sandhya-kulkarni <mails4sandhya@gmail.com>
2022-12-10 15:39:39 +0000

    added emnist dataloader

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -14,3 +14,18 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class EmnistDataLoader(BaseDataLoader):
+    """
+    EMNIST data loading demo using BaseDataLoader
+    """
+    def __init__(self, data_dir, split, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        trsfe = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+        self.data_dir = data_dir
+        self.split = split
+        self.dataset = datasets.EMNIST(self.data_dir, self.split, train=training, download=True, transform=trsfe)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)

37a58ed1836b46d5580f4e3123643e61e2921bf9
sandhya-kulkarni <mails4sandhya@gmail.com>
2022-12-12 12:29:59 +0000

    Emnist dataset and model added

--- model/model.py
+++ model/model.py
@@ -1,6 +1,8 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
+import torchvision.models as models
 
 
 class MnistModel(BaseModel):
@@ -20,3 +22,21 @@ class MnistModel(BaseModel):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+
+# A simple custom 2 layers CNN model
+class EmnistModel(BaseModel):
+    def __init__(self, num_classes=47):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, 5, 1)
+        self.fc = nn.Linear(64 * 20 * 20, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 1)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        output = F.log_softmax(x, dim=1)
+        return output
